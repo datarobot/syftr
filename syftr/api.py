0876fba700b9715a76a8f8b10c78e625aabcc2d2
nvdv <nick.volynets@gmail.com>
2025-05-27 13:46:39 +0200

    Refine CLI descriptions slightly.

--- syftr/api.py
+++ syftr/api.py
@@ -313,8 +313,8 @@ class Study:
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(
-        description="This is the syftr API CLI for running syftr studies.",
-        epilog="Verify the study config file before running. Significant costs may occur.",
+        description="This is the syftr API CLI, the main entry point for running syftr in the shell.",
+        epilog="Please, verify study configs before submission as it may incur significant costs.",
     )
 
     parser.add_argument("--study-config", help="Path to study config yaml")
