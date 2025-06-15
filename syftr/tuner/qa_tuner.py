0e9c15484cbac8a88312c9b0070ffc9e586a30b3
Matthew Hausknecht <matthew.hausknecht@datarobot.com>
2025-06-15 12:17:41 -0500

    Cache updates

--- syftr/tuner/qa_tuner.py
+++ syftr/tuner/qa_tuner.py
@@ -140,7 +140,7 @@ def build_flow(params: T.Dict, study_config: StudyConfig) -> Flow:
             additional_context_num_nodes=params.get("additional_context_num_nodes", 0),
             params=params,
             enforce_full_evaluation=enforce_full_evaluation,
-            retriever_fingerprint=get_retriever_fingerprint(study_config, params),
+            retriever_cache_fingerprint=get_retriever_fingerprint(study_config, params),
         )
 
     get_qa_examples = None
@@ -174,6 +174,7 @@ def build_flow(params: T.Dict, study_config: StudyConfig) -> Flow:
             enforce_full_evaluation=enforce_full_evaluation,
         )
     else:
+        retriever_cache_fingerprint = get_retriever_fingerprint(study_config, params)
         hyde_llm = reranker_llm = reranker_top_k = None
         if params.get("hyde_enabled"):
             hyde_llm = get_llm(params["hyde_llm_name"])
@@ -201,6 +202,7 @@ def build_flow(params: T.Dict, study_config: StudyConfig) -> Flow:
                     additional_context_num_nodes=additional_context_num_nodes,
                     enforce_full_evaluation=enforce_full_evaluation,
                     params=params,
+                    retriever_cache_fingerprint=retriever_cache_fingerprint,
                 )
             case "react_rag_agent":
                 subquestion_engine_llm = get_llm(params["subquestion_engine_llm"])
@@ -225,6 +227,7 @@ def build_flow(params: T.Dict, study_config: StudyConfig) -> Flow:
                     dataset_description=study_config.dataset.description,
                     enforce_full_evaluation=enforce_full_evaluation,
                     params=params,
+                    retriever_cache_fingerprint=retriever_cache_fingerprint,
                 )
             case "critique_rag_agent":
                 subquestion_engine_llm = get_llm(params["subquestion_engine_llm"])
@@ -253,6 +256,7 @@ def build_flow(params: T.Dict, study_config: StudyConfig) -> Flow:
                     dataset_description=study_config.dataset.description,
                     enforce_full_evaluation=enforce_full_evaluation,
                     params=params,
+                    retriever_cache_fingerprint=retriever_cache_fingerprint,
                 )
             case "sub_question_rag":
                 subquestion_engine_llm = get_llm(params["subquestion_engine_llm"])
@@ -275,6 +279,7 @@ def build_flow(params: T.Dict, study_config: StudyConfig) -> Flow:
                     dataset_description=study_config.dataset.description,
                     enforce_full_evaluation=enforce_full_evaluation,
                     params=params,
+                    retriever_cache_fingerprint=retriever_cache_fingerprint,
                 )
             case "lats_rag_agent":
                 flow = LATSAgentFlow(
@@ -293,6 +298,7 @@ def build_flow(params: T.Dict, study_config: StudyConfig) -> Flow:
                     max_rollouts=params["lats_max_rollouts"],
                     enforce_full_evaluation=enforce_full_evaluation,
                     params=params,
+                    retriever_cache_fingerprint=retriever_cache_fingerprint,
                 )
             case _:
                 raise ValueError(f"Invalid rag_mode: {params['rag_mode']}")
