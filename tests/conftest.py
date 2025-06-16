bd59d1ac4557feaa3e5c22f271a783d956f26935
Stefan Hackmann <stefan.hackmann@datarobot.com>
2025-06-16 14:10:12 +0000

    Do not share ray tmp directory with head node

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,3 +1,8 @@
+import os
+import tempfile
+
+import pytest
+
 from syftr.huggingface_helper import load_hf_token_into_env
 from syftr.startup import download_nltk_data
 
@@ -7,3 +12,10 @@ load_hf_token_into_env()
 
 def pytest_addoption(parser):
     parser.addoption("--gpu", action="store_true", help="Use GPU for embeddings")
+
+
+@pytest.fixture(scope="session", autouse=True)
+def set_ray_tmpdir():
+    with tempfile.TemporaryDirectory() as tmpdirname:
+        os.environ["RAY_TMPDIR"] = tmpdirname
+        yield
