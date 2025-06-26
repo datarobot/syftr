e093bec3d4d7be998a8e0cc72ce36d29d86a2b3e
Stefan Hackmann <stefan.hackmann@datarobot.com>
2025-06-26 09:30:19 +0000

    adding suffix and substring

--- syftr/experiments.py
+++ syftr/experiments.py
@@ -20,16 +20,27 @@ async def iter_all_job_logs(tailers: T.List[T.AsyncIterable]):
     await asyncio.gather(*log_iters)
 
 
-def attach_logs(prefix: str = "<doesntmatch>"):  # , remote: bool = True
-    # cfg.ray.local = False if remote else cfg.ray.local
+def attach_logs(
+    prefix: str = "<doesntmatch>",
+    suffix: str = "<doesntmatch>",
+    substring: str = "<doesntmatch>",
+):
+    """Connect to all jobs that match the given prefix, suffix, or substring"""
     client = get_client()
     job_details = client.list_jobs()
     jobs_to_tail = [
         job
         for job in job_details
-        if job.submission_id is not None
-        and job.submission_id.startswith(prefix)
-        and job.status not in {JobStatus.STOPPED, JobStatus.SUCCEEDED, JobStatus.FAILED}
+        if (
+            job.submission_id is not None
+            and (
+                job.submission_id.startswith(prefix)
+                or job.submission_id.endwith(suffix)
+                or substring in job.submission_id
+            )
+            and job.status
+            not in {JobStatus.STOPPED, JobStatus.SUCCEEDED, JobStatus.FAILED}
+        )
     ]
     log_tailers = [client.tail_job_logs(job.job_id) for job in jobs_to_tail]
     asyncio.run(iter_all_job_logs(log_tailers))
