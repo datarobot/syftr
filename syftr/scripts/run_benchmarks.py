d74eb5484d1591afcbfdd961963f222965e1fe79
Stefan Hackmann <stefan.hackmann@datarobot.com>
2025-06-28 04:39:29 +0000

    backup

--- syftr/scripts/run_benchmarks.py
+++ syftr/scripts/run_benchmarks.py
@@ -57,31 +57,30 @@ from syftr.studyconfig_helper import build_configs
 # -------------------------------------------------------
 PREFIX = "silver"  # this three parameters
 BENCH_NUM = 1  # are used to name
-# RUN_NAME = "in-sample"  # your config files and studies
-RUN_NAME = "out-of-sample"
+RUN_NAME = "in-sample"  # your config files and studies
+# RUN_NAME = "out-of-sample"
 # -------------------------------------------------------
-# NUM_TRIALS = 0  # total number of optimization trials per submission
-NUM_TRIALS = 400  # total number of optimization trials per submission
+NUM_TRIALS = 0  # total number of optimization trials per submission
+# NUM_TRIALS = 50  # total number of optimization trials per submission
 REUSE_STUDY = True  # WARNING: if set to False, exsting studies will be deleted!
-RECREATE_STUDY = True  # if set to True, recreating an existing study without failed or running trials
+RECREATE_STUDY = (
+    False  # WARNING: do not use with simultaneous runs using the same study!
+)
 EVAL_MODE: T.Literal["single", "random", "consensus"] = "random"
 DRY_RUN = False  #  a dry run will not submit jobs but create the study configs
 EMBEDDING_MAX_TIME = 3600 * 8
-MINUTES_BEFORE_NEXT_SUBMISSION = 3
-# CUSTOM_BASELINES = "all"  # "pareto", "all", "silver", None
-CUSTOM_BASELINES = None  # "pareto", "all", "silver", None
+MINUTES_BEFORE_NEXT_SUBMISSION = 1
+CUSTOM_BASELINES = "all"  # "pareto", "all", "silver", None
+# CUSTOM_BASELINES = None  # "pareto", "all", "silver", None
 BASELINES_BATCH_SIZE = 100  # we require batching of baselines to avoid Ray OOM issues
 BASELINES_START = 0  # you can restrict the number of baselines ...
-BASELINES_END = 200  # ... to start with here to avoid OOM issues
-STOP_AFTER_ONE_BATCH_OF_BASELINES = (
-    False  # useful when recreating studies and using a lot of baselines
-)
+BASELINES_END = 100  # ... to start with here to avoid OOM issues
 # -------------------------------------------------------
 BASELINE_STUDIES: T.List[str] = [
     "silver1--in-sample--bright_hf--earth_science",
     "silver1--in-sample--bright_hf--economics",
-    # "silver1--in-sample--bright_hf--pony",
-    # "silver1--in-sample--bright_hf--psychology",
+    "silver1--in-sample--bright_hf--pony",
+    "silver1--in-sample--bright_hf--psychology",
     "silver1--in-sample--bright_hf--robotics",
     "silver1--in-sample--bright_hf--sustainable_living",
 ]
@@ -260,20 +259,20 @@ DATASETS = [
     # CragTask3HF(subset="sports"),
     # -----------------------------------------------
     # DRDocsHF(),
-    FinanceBenchHF(),
-    HotPotQAHF(subset="train_hard"),
-    InfiniteBenchHF(),
-    MultiHopRAGHF(),
-    PhantomWikiv050(),
+    # FinanceBenchHF(),
+    # HotPotQAHF(subset="train_hard"),
+    # InfiniteBenchHF(),
+    # MultiHopRAGHF(),
+    # PhantomWikiv050(),
     # -----------------------------------------------
     # BrightHF(subset="stackoverflow"),
     # BrightHF(subset="pony"),
     # BrightHF(subset="psychology"),
     # -----------------------
-    # BrightHF(subset="earth_science"),
-    # BrightHF(subset="economics"),
-    # BrightHF(subset="robotics"),
-    # BrightHF(subset="sustainable_living"),
+    BrightHF(subset="earth_science"),
+    BrightHF(subset="economics"),
+    BrightHF(subset="robotics"),
+    BrightHF(subset="sustainable_living"),
 ]
 assert DATASETS, "No datasets found. Please check the dataset list."
 
@@ -319,9 +318,6 @@ def get_optimization_parameters():
             optimization_config = optimization_config.model_copy()
             optimization_config.baselines = BASELINES[i : i + BASELINES_BATCH_SIZE]
             yield DATASETS, SEARCH_SPACE, optimization_config, EVALUATION
-            if STOP_AFTER_ONE_BATCH_OF_BASELINES:
-                logger.warning("Stopping after one batch of baselines")
-                return  # single iteration through datasets with first batch of baselines
     else:
         yield DATASETS, SEARCH_SPACE, optimization_config, EVALUATION
 
