98f3f55bb7f07f9ff5428fcdf086428afa07d51b
Stefan Hackmann <stefan.hackmann@datarobot.com>
2025-07-21 14:19:46 +0000

    comparing all judge types

--- syftr/scripts/experiments/test_judge_flow.py
+++ syftr/scripts/experiments/test_judge_flow.py
@@ -6,14 +6,15 @@ import yaml
 from syftr.ray.submit import get_client, start_study, tail
 from syftr.storage import JudgeEvalHF
 from syftr.studies import (
+    ConsensusCorrectnessEvaluator,
     Evaluation,
     JudgeSearchSpace,
     OptimizationConfig,
+    RandomCorrectnessEvaluator,
     SingleCorrectnessEvaluator,
     StudyConfig,
+    get_llm_name_combinations,
 )
-
-N_JUDGES: T.List[int] = [3, 5]
 
 JUDGE_LLMS: T.List[str] = [
     "Qwen/Qwen2.5",
@@ -26,9 +27,7 @@ JUDGE_LLMS: T.List[str] = [
 
 
 def main():
-    # name = "judge-eval-consensus"
-    # name = "judge-eval-study-14-simplefix"
-    name = "judge-eval-simple"
+    name = "judge-eval-all"
     study_config = StudyConfig(
         name=name,
         reuse_study=False,
@@ -36,32 +35,32 @@ def main():
         dataset=JudgeEvalHF(),
         evaluation=Evaluation(mode="judge"),
         search_space=JudgeSearchSpace(
-            judge_prompts=["simple"],
-            # judge_prompts=["detailed", "comparison", "simple"],
+            judge_prompts=["detailed", "comparison", "simple"],
             single_correctness_evaluator=SingleCorrectnessEvaluator(
                 response_synthesizer_llms=JUDGE_LLMS
             ),
-            # consensus_correctness_evaluator=ConsensusCorrectnessEvaluator(
-            #     response_synthesizer_llms=JUDGE_LLMS,
-            #     response_synthesizer_llm_combinations=get_llm_name_combinations(
-            #         JUDGE_LLMS, [3, 5]
-            #     ),
-            # ),
-            # random_correctness_evaluator=RandomCorrectnessEvaluator(
-            #     response_synthesizer_llms=JUDGE_LLMS,
-            #     response_synthesizer_llm_combinations=get_llm_name_combinations(
-            #         JUDGE_LLMS, [3, 5]
-            #     ),
-            # ),
+            consensus_correctness_evaluator=ConsensusCorrectnessEvaluator(
+                response_synthesizer_llms=JUDGE_LLMS,
+                response_synthesizer_llm_combinations=get_llm_name_combinations(
+                    JUDGE_LLMS, [2, 3, 4, 5]
+                ),
+            ),
+            random_correctness_evaluator=RandomCorrectnessEvaluator(
+                response_synthesizer_llms=JUDGE_LLMS,
+                response_synthesizer_llm_combinations=get_llm_name_combinations(
+                    JUDGE_LLMS, [2, 3, 4, 5]
+                ),
+            ),
         ),
         optimization=OptimizationConfig(
-            num_trials=150,
+            num_trials=1500,
             baselines=[],
-            num_random_trials=8,
+            num_random_trials=10,
             use_individual_baselines=False,
             use_agent_baselines=False,
             use_variations_of_baselines=False,
-            max_concurrent_trials=10,
+            max_concurrent_trials=50,
+            num_eval_samples=1000,
             num_eval_batch=10,
             max_eval_failure_rate=0.05,
         ),
